ae1e6d532a0bad657801e8affb9c2159f676ce50
Sacha Arseneault <sacha.arseneault@gmail.com>
2023-10-16 09:31:30 -0400

    chore: update hierarchy

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,16 @@
-import Hero from "../Hero/Hero";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <>
-      <Hero />
-    </>
+    <div className="flex justify-center">
+      <div className=" w-1/2 border border-red-500 text-center">
+        <h1 className="text-3xl font-bold text-blue-950">Hero Section</h1>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
+    </div>
   );
 }
 
