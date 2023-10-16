48b10aceda61fe8a9f483d4b7f2f8763b29114bc
Sacha Arseneault <sacha.arseneault@gmail.com>
2023-10-16 17:26:08 -0400

    feat: openCV integration for pattern recognition, currently setup with dummy image

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,34 +4,44 @@ import Home from "../Home/Home.jsx";
 import Selection from "../Selection/Selection.jsx";
 import Settings from "../Settings/Settings.jsx";
 import ReadyPage from "../ReadyPage/ReadyPage.jsx";
+import Camera from "../Camera/Camera.jsx";
 import ResultPage from "../ResultPage/ResultPage.jsx";
 
 function App() {
   const [pageIndex, setPageIndex] = useState(0);
-  const pages = [<Home/>, <Selection/>, <Settings/>,<ReadyPage/>,<ResultPage/>]
+  const pages = [
+    <Home key="home" />,
+    <Selection key="selection" />,
+    <Settings key="settings" />,
+    <ReadyPage key="readyPage" />,
+    <Camera key="camera" />,
+    <ResultPage key="resultPage" />,
+  ];
 
+  const nextPage = () => {
+    if (pageIndex != pages.length - 1)
+      setPageIndex((pageIndex) => pageIndex + 1);
+  };
 
-  const nextPage = () =>{
-    if (pageIndex != pages.length-1)
-      setPageIndex(pageIndex => (pageIndex + 1));
-  }
-
-  const previousPage = () =>{
-    if (pageIndex != 0)
-      setPageIndex(pageIndex => (pageIndex - 1));
-  }
-
+  const previousPage = () => {
+    if (pageIndex != 0) setPageIndex((pageIndex) => pageIndex - 1);
+  };
 
   return (
     <>
       <div className="h-full w-full flex">
         <div className="border border-red h-48 w-48">
-          <button className="border border-black text-black" onClick={previousPage}>Previous Page</button>
-          <button className="border border-black text-black" onClick={nextPage}>Next Page</button>
+          <button
+            className="border border-black text-black"
+            onClick={previousPage}
+          >
+            Previous Page
+          </button>
+          <button className="border border-black text-black" onClick={nextPage}>
+            Next Page
+          </button>
         </div>
-        <div className="content">
-          {pages[pageIndex]}
-        </div>
+        <div className="content">{pages[pageIndex]}</div>
       </div>
     </>
   );
---
+++ src/components/Camera/Camera.jsx
@@ -0,0 +1,102 @@
+/* eslint-disable no-unused-vars */
+import Webcam from "react-webcam";
+import { useCallback, useRef, useState } from "react";
+import { calculateResult } from "../../processing.js";
+import sample_pos from "../../assets/sample_pos.png";
+import sample_neg from "../../assets/sample_neg.png";
+import sample_inv from "../../assets/sample_inv.png";
+
+function Camera() {
+  const webcamRef = useRef(null); //webcam reference
+  const [imgSrc, setImgSrc] = useState(null); //init
+  const [outputImg, setOutputImg] = useState(null); //init
+
+  const [resultText, setResultText] = useState(null); //init
+  const [resultAccuracy, setResultAccuracy] = useState(null); //init
+
+  const value_chart = {
+    0: "Positive",
+    1: "Negative",
+    2: "Invalid",
+    3: "Not recognized",
+  };
+
+  //capture function
+  const capture = useCallback(() => {
+    const imageSource = webcamRef.current.getScreenshot();
+    setImgSrc(imageSource);
+    //timeout
+    setTimeout(() => {
+      processImage(imageSource);
+    }, 1000);
+  }, [webcamRef]);
+
+  const retake = () => {
+    setImgSrc(null); //reset image
+  };
+
+  const processImage = (imgSrc) => {
+    const result = calculateResult(imgSrc);
+    setResultText(value_chart[result[0]]);
+    setResultAccuracy(result[1]);
+  };
+
+  return (
+    <div className="container">
+      {imgSrc ? (
+        <img
+          id="img_src"
+          src={imgSrc}
+          alt="taken_image"
+          width="600"
+          height="600"
+        />
+      ) : (
+        <Webcam height={600} width={600} ref={webcamRef} />
+      )}
+      <div className="btn-container">
+        {imgSrc ? (
+          <button onClick={retake}>Retake photo</button>
+        ) : (
+          <button onClick={capture}>Capture photo</button>
+        )}
+      </div>
+      <h1>{resultText}</h1>
+      <h2>{resultAccuracy}</h2>
+      <img
+        id="pos_sample"
+        src="src/assets/sample_pos.png"
+        alt="pos sample"
+        width="250"
+        height="300"
+        className="hidden"
+      />
+      <img
+        id="neg_sample"
+        src="src/assets/sample_neg.png"
+        alt="neg sample"
+        width="250"
+        height="300"
+        className="hidden"
+      />
+      <img
+        id="inv_sample"
+        src="src/assets/sample_inv.png"
+        alt="inv sample"
+        width="250"
+        height="300"
+        className="hidden"
+      />
+      <img
+        id="dummy_img"
+        src="src/assets/positive_test.png"
+        alt="dummy img"
+        width="300"
+        height="900"
+        className="hidden"
+      />
+    </div>
+  );
+}
+
+export default Camera;
