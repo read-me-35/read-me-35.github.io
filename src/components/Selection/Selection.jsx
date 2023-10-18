43d69e92c044dd60f424b14d5d4a31edab6c6c74
HenrySaber <hsabe026@uottawa.ca>
2023-10-18 13:33:55 -0400

    Feat: Added dark mode and light mode and added pictures to every button, added button fonctunality

--- src/components/Selection/Selection.jsx
+++ src/components/Selection/Selection.jsx
@@ -1,35 +1,34 @@
-function Selection() {
+import covid from "../../assets/images/covidimage.png";
+import ph from "../../assets/images/phimage.png";
+import preg from "../../assets/images/pregnancyimage.webp";
+
+function Selection(props) {
   return (
-    <div className="bg-gray-900 text-white h-screen flex flex-col justify-center items-center">
-      <div className="flex-1 flex flex-col justify-center items-center">
-        <button className="bg-red-500 text-white px-8 py-4 rounded-lg flex flex-col items-center space-y-2">
-          <span>Back</span>
+    <div className="bg-transparent text-white h-screen flex flex-col justify-center items-center">
+      <div className="flex-1 flex flex-col justify-center items-center space-y-4 wrap">
+        <button className="bg-red-500 text-white px-4 py-2 rounded-lg w-36 sm:w-40 flex flex-col items-center space-y-2" onClick={props.toPrevPage}>
+          <span className="text-center text-base sm:text-lg">Back</span>
+        </button>
+        <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg w-36 sm:w-40 flex flex-col items-center space-y-2" onClick={props.toNextPage}>
+          <span className="text-center text-base sm:text-lg">Covid</span>
           <img
-            src="path-to-your-image.png" // Replace with the actual path to your image
+            src={covid}
             alt="Icon"
             className="w-6 h-6"
           />
         </button>
-        <button className="bg-yellow-500 text-white px-8 py-4 rounded-lg flex flex-col items-center space-y-2">
-          <span>Covid</span>
+        <button className="bg-pink-500 text-white px-4 py-2 rounded-lg w-36 sm:w-40 flex flex-col items-center space-y-2">
+          <span className="text-center text-base sm:text-lg">Pregnancy</span>
           <img
-            src="path-to-your-image.png" // Replace with the actual path to your image
+            src={preg}
             alt="Icon"
             className="w-6 h-6"
           />
         </button>
-        <button className="bg-pink-500 text-white px-8 py-4 rounded-lg flex flex-col items-center space-y-2">
-          <span>Pregancy</span>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg w-36 sm:w-40 flex flex-col items-center space-y-2">
+          <span className="text-center text-base sm:text-lg">PH Level</span>
           <img
-            src="path-to-your-image.png" // Replace with the actual path to your image
-            alt="Icon"
-            className="w-6 h-6"
-          />
-        </button>
-        <button className="bg-blue-500 text-white px-8 py-4 rounded-lg flex flex-col items-center space-y-2">
-          <span>PH Level</span>
-          <img
-            src="path-to-your-image.png" // Replace with the actual path to your image
+            src={ph}
             alt="Icon"
             className="w-6 h-6"
           />
--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,35 +1,30 @@
-function Selection() {
-    return (
-      <div className="bg-gray-900 text-white h-screen flex flex-col justify-center items-center">
-        <div className="flex-1 flex flex-col justify-center items-center">
-          <button className="bg-red-500 text-white px-8 py-4 rounded-lg flex flex-col items-center space-y-2">
-            <span>Back</span>
-            <img
-              src="path-to-your-image.png" // Replace with the actual path to your image
-              alt="Icon"
-              className="w-6 h-6"
-            />
-          </button>
-          <button className="bg-gray-100 text-black px-8 py-4 rounded-lg flex flex-col items-center space-y-2">
-            <span>Light</span>
-            <img
-              src="path-to-your-image.png" // Replace with the actual path to your image
-              alt="Icon"
-              className="w-6 h-6"
-            />
-          </button>
-          <button className="bg-gray-700 text-white px-8 py-4 rounded-lg flex flex-col items-center space-y-2">
-            <span>Dark</span>
-            <img
-              src="path-to-your-image.png" // Replace with the actual path to your image
-              alt="Icon"
-              className="w-6 h-6"
-            />
-          </button>
-        </div>
+import dark from "../../assets/images/moonimage.png";
+import light from "../../assets/images/lightimage.png";
+function Selection(props) {
+  const { isLightMode, toggleMode } = props;
+
+  return (
+    <div className={` text-white h-screen flex flex-col justify-center items-center bg-transparent`}>
+      <div className="flex-1 flex flex-col justify-center items-center space-y-4">
+        <button className="bg-red-500 text-white px-4 py-2 rounded-lg w-32 flex flex-col items-center space-y-2" onClick={props.toNextPage}>
+          <span className="text-center">Back</span>
+        </button>
+        <div className="my-2"></div> {/* Add space between buttons */}
+        <button className={`${isLightMode? "bg-gray-700 text-white": "bg-white text-black"}  px-4 py-2 rounded-lg w-48 flex flex-col items-center space-y-2`} onClick={props.toggleDisplayMode}>
+          <span className="text-center">{isLightMode? "Dark": "Light"}</span>
+          {isLightMode? <img
+          src={dark}
+          alt="Icon"
+          className="w-8 h-8"
+        />: <img
+        src={light}
+        alt="Icon"
+        className="w-8 h-8"
+      />}
+        </button>
       </div>
-    );
-  }
-  
-  export default Selection;
-  
+    </div>
+  );
+}
+
+export default Selection;
